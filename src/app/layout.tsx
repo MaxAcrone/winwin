5549f725b72eaf3ee4103dcc08cb2238bb7fc58b
MaxAcrone <beedbeedmax@gmail.com>
2026-03-17 23:40:46 +0500

    feat: add Yandex Metrika tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,21 @@ export default function RootLayout({
     <html lang="ru">
       <head>
         <Script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js" strategy="beforeInteractive" />
+        {/* Yandex.Metrika counter */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=107703417', 'ym');
+            ym(107703417, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        <noscript>
+          <div><img src="https://mc.yandex.ru/watch/107703417" style={{position:'absolute', left:'-9999px'}} alt="" /></div>
+        </noscript>
       </head>
       <body className="w-full font-mono text-sm antialiased">
         {children}
